1eba40e7737890e2ca45827681ea12bf6d87c765
Yosef Gehasy <djedjehasi@gmail.com>
2022-12-20 14:36:27 +0200

    service section

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,8 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'hero-pattern':
-          "linear-gradient(to right bottom, rgba(34,193,195,0.65172006302521), rgba(253,187,45,0.6685267857142857)), url('../public/dkmhero.jpeg')",
-        // background: rgb(34,193,195);
-        //background: linear-gradient(156deg, rgba(34,193,195,0.65172006302521) 0%, rgba(253,187,45,0.6685267857142857) 100%);
+        'hero-pattern': "linear-gradient(to right bottom, rgba(34,193,195,0.65172006302521), rgba(253,187,45,0.6685267857142857)), url('../public/dkmhero.jpeg')",
+        'service-patten': "linear-gradient(to right bottom, rgba(34,193,195,0.85), rgba(253,187,45,0.85)), url('../public/dkmservice.jpeg')"
       },
     },
   },
