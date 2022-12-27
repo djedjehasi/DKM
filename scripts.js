e71204b00af525a334dd670ffc0e0b70d983bfa8
Yosef Gehasy <djedjehasi@gmail.com>
2022-12-27 04:48:35 +0200

    again

--- scripts.js
+++ scripts.js
@@ -12,10 +12,12 @@ const buttomLine = document.querySelector('#line_b');
 hamburger.addEventListener('click', ()=> {
     header.classList.toggle('shadow-md')
     nav.classList.toggle('opacity-100');
+    nav.classList.toggle('hidden');
     //nav.classList.toggle('top-20');
     topLine.classList.toggle('rotate-45');
     topLine.classList.toggle('translate-y-2.5');
     middleLine.classList.toggle('opacity-0');
+
     middleLine.classList.toggle('-translate-x-1');
     buttomLine.classList.toggle('-rotate-45');
     buttomLine.classList.toggle('-translate-y-2.5');
